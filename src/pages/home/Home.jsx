f21ed6600d679097dda87f2dc5a6ee164624f79c
rikesh mahat <rikeshmahat.96@gmail.com>
2024-02-20 16:29:51 +0545

    collection page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from './Banner.jsx'
 import Category from './Category.jsx'
 import Products from './Products.jsx'
+import Collections from './Collections.jsx'
 function Home() {
   return (
     <div>
       <Banner />
       <Category />
       <Products/>
+      <Collections/>
     </div>
   )
 }

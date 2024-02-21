d339c31eec706cc8cbd4760847c8e5f5d730e086
rikesh mahat <rikeshmahat.96@gmail.com>
2024-02-21 13:28:48 +0545

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>

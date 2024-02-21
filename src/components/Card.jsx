a702bc5ea8c692dec5b04c289eb482c81f3b3726
rikesh mahat <rikeshmahat.96@gmail.com>
2024-02-21 13:27:21 +0545

    small fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 function Card({products}) {
 
-  console.log(products)
+
   return (
     <div className='max-h-90 grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-3 items-center justify-center gap-4 px-4 shadow-sm outline-none border-none'>{
         products.map((item) => {
 
           return(
             <div key={item.id} className='border-2'>
-                <Link to={`/shop/${item.id}`}>
+                <Link to={`/product/${item.id}`}>
                   
                 
                     <img src={item.image} alt={item.id} className='w-full mx-auto hover:scale-110 duration-500'/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
         {title : "Craft, Supplies & Tools" , path : "/"},
     ]
   return (
-    <header className='max-w-screen-2xl xl:px-28 px-4 absolute top-0 right-0 left-0'>
+    <header className='max-w-screen-2xl mx-auto xl:px-28 px-4 absolute top-0 right-0 left-0'>
         <nav className='flex justify-between items-center container md:py-4 pt-6 pb-3'>
         <FaSearch className='text-black w-5 h-5 cursor-pointer hidden md:block' />
         <a href="/"><img src={logo} alt="" /></a>
